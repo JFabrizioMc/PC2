463e8c4dcd66c2015be30b7881ca409852ad0547
Jose Fabrizio <josefabriziomanco@gmail.com>
2018-11-20 07:43:46 -0500

    E01

---
+++ PC2_01.py
@@ -0,0 +1,16 @@
+import math
+# Entrada 
+x1 = 0
+y1 = 0
+
+x2 = int(input("Ingrese la coordenada en el eje x\n"))
+y2 = int(input("Ingrese la coordenada en el eje y\n"))
+
+coordenada = [x2,y2]
+
+# Proceso
+
+distancia = math.sqrt((x2 - x1)*(x2 - x1) + (y2 - y1)*(y2 - y1)) 
+
+print ("Coordenada :", coordenada)
+print("La distancia entre [0,0] y ", coordenada, " es :", round(distancia))
